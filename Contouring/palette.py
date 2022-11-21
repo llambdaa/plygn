66ee4211331c25bdc535fd4efed5dc60f643f92e
Lukas Rapp <lu.rapp@web.de>
2022-11-21 09:37:54 +0100

    Optimized construction of label expansion lookup table

--- Contouring/palette.py
+++ Contouring/palette.py
@@ -5,23 +5,20 @@ import numpy as np
 KMEANS_ITERATIONS = 100
 KMEANS_RUNS = 10
 
-from utils import *
 
 def kmeans(clusters, points, weights):
     process = faiss.Kmeans(d=points.shape[1], k=clusters, niter=KMEANS_ITERATIONS, nredo=KMEANS_RUNS)
     process.train(points, weights)
     result = process.index.search(points, 1)[1]
+    result = result.ravel()
     return result
 
 
 def expand_labels(image_as_ints, unique_ints, labels, shape):
     # Each unique color is represented as an int
     # and is used as an index to its own label.
-    lookup = np.empty((256 ** 3), dtype=np.uint8)
-    for i in range(len(unique_ints)):
-        index = unique_ints[i]
-        label = labels[i]
-        lookup[index] = label
+    lookup = np.empty((256 ** 3), dtype=np.int32)
+    np.put(lookup, unique_ints, labels)
 
     # Each image pixel is also represented as an int.
     # Its value is used to index the lookup table,
