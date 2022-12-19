6499094e7b2212151b85d98a73c72940750a227c
Lukas Rapp <lu.rapp@web.de>
2022-12-19 16:56:07 +0100

    Fixed missing format exporter

--- export.py
+++ export.py
@@ -1,28 +1,42 @@
+import qoi
 from enum import Enum
 
 
 class ExportFormat(Enum):
-    JPG = 'jpg'
-    PNG = 'png'
-    QOI = 'qoi'
+    JPG = 'JPG'
+    PNG = 'PNG'
+    QOI = 'QOI'
 
     def __str__(self):
         return self.value
 
 
-def __export(path, processed, original, export_format, export_original):
-    cv2.imwrite(
-        f"{path}_processed.{export_format.value}",
-        cv2.cvtColor(processed, cv2.COLOR_RGB2BGR)
-    )
-
-    if export_original:
+def export(path, processed, original, export_formats, export_original):
+    if ExportFormat.JPG in export_formats:
         cv2.imwrite(
-            f"{path}_original.{export_format.value}",
-            cv2.cvtColor(original, cv2.COLOR_RGB2BGR)
+            f"{path}_processed.jpg",
+            cv2.cvtColor(processed, cv2.COLOR_RGB2BGR)
         )
 
+        if export_original:
+            cv2.imwrite(
+                f"{path}_original.jpg",
+                cv2.cvtColor(original, cv2.COLOR_RGB2BGR)
+            )
 
-def export(path, processed, original, export_formats, export_original):
-    for specific in export_formats:
-        __export(path, processed, original, specific, export_original)
+    if ExportFormat.PNG in export_formats:
+        cv2.imwrite(
+            f"{path}_processed.png",
+            cv2.cvtColor(processed, cv2.COLOR_RGB2BGR)
+        )
+
+        if export_original:
+            cv2.imwrite(
+                f"{path}_original.png",
+                cv2.cvtColor(original, cv2.COLOR_RGB2BGR)
+            )
+
+    if ExportFormat.QOI in export_formats:
+        qoi.write(f"{path}_processed.qoi", processed)
+        if export_original:
+            qoi.write(f"{path}_original.qoi", original)
