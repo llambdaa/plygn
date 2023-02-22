143acf31200cca5df2dac7c36b71e5d8cb3d2c5b
Lukas Rapp <lu.rapp@web.de>
2023-02-22 16:32:18 +0100

    Renamed flag -O (original) to -U (unprocessed)

--- export.py
+++ export.py
@@ -12,17 +12,17 @@ class ExportFormat(Enum):
         return self.value
 
 
-def export(path, processed, original, export_formats, export_original):
+def export(path, processed, unprocessed, export_formats, export_unprocessed):
     if ExportFormat.JPG in export_formats:
         cv2.imwrite(
             f"{path}_processed.jpg",
             cv2.cvtColor(processed, cv2.COLOR_RGB2BGR)
         )
 
-        if export_original:
+        if export_unprocessed:
             cv2.imwrite(
-                f"{path}_original.jpg",
-                cv2.cvtColor(original, cv2.COLOR_RGB2BGR)
+                f"{path}_unprocessed.jpg",
+                cv2.cvtColor(unprocessed, cv2.COLOR_RGB2BGR)
             )
 
     if ExportFormat.PNG in export_formats:
@@ -31,13 +31,13 @@ def export(path, processed, original, export_formats, export_original):
             cv2.cvtColor(processed, cv2.COLOR_RGB2BGR)
         )
 
-        if export_original:
+        if export_unprocessed:
             cv2.imwrite(
-                f"{path}_original.png",
-                cv2.cvtColor(original, cv2.COLOR_RGB2BGR)
+                f"{path}_unprocessed.png",
+                cv2.cvtColor(unprocessed, cv2.COLOR_RGB2BGR)
             )
 
     if ExportFormat.QOI in export_formats:
         qoi.write(f"{path}_processed.qoi", processed)
-        if export_original:
-            qoi.write(f"{path}_original.qoi", original)
+        if export_unprocessed:
+            qoi.write(f"{path}_unprocessed.qoi", unprocessed)
