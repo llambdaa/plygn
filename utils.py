cf430e113c3b3d877578141346e1872cdd39e376
Lukas Rapp <lu.rapp@web.de>
2023-02-22 12:34:40 +0100

    Added process start message with truncated image path

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 
+from pathlib import Path
 
 def time():
     return datetime.datetime.now()
@@ -13,3 +14,12 @@ def make_folder(out_path, specification):
         os.mkdir(composed)
 
     return composed
+
+
+def truncate_path(target, length):
+    path = Path(target)
+    parts = path.parts
+    if length >= len(parts):
+        return path
+    
+    return Path(".../", *parts[-length:])
