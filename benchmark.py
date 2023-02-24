a55a4f65b010755868c23f5e4ef027b80c64329d
Lukas Rapp <lu.rapp@web.de>
2023-02-24 10:12:31 +0100

    Refactored benchmarking and introduced MS-SSIM measurement

--- benchmark.py
+++ benchmark.py
@@ -1,73 +1,98 @@
 import os
-
-from sewar.full_ref import uqi
+import torch
+import numpy as np
 
 from plygn import load_image
 from export import *
+from enum import Enum
+from pytorch_msssim import ms_ssim
+
+
+class PartialResultType(Enum):
+    PROCESSED = "processed"
+    UNPROCESSED = "unprocessed"
+
+    def __str__(self):
+        return self.value
+
+
+def get_image_tensor(input_path):
+    _, image = load_image(input_path)
+    image = np.transpose(image, (2, 0, 1))
+    image = np.expand_dims(image, axis=0)
+    image = image.astype(np.float32)
+    image = torch.from_numpy(image)
+    return image
+
+
+def write_partial_result(path, partial_result_type, original_data, format_result):
+    path = path.format(partial_result_type)
+    image = get_image_tensor(path)
+    size = os.path.getsize(path)
+    original_image = original_data[0]
+    original_size = original_data[1]
+    similarity_to_original = ms_ssim(original_image, image, data_range=255).item()
+
+    partial_result = {
+        "path": path,
+        "size": size,
+        "size_ratio_to_original": size / original_size,
+        "similarity_to_original": similarity_to_original
+    }
+
+    format_result[str(partial_result_type)] = partial_result
+
+
+def write_partial_result_comparison(format_result):
+    processed_result = format_result[str(PartialResultType.PROCESSED)]
+    unprocessed_result = format_result[str(PartialResultType.UNPROCESSED)]
+
+    # Both image format conversion/compression with and without
+    # prior triangulation are performed. Both partial results
+    # can now be compared in terms of size and similarity with
+    # respect to the shared common original.
+    processed_size = processed_result["size"]
+    unprocessed_size = unprocessed_result["size"]
+    format_result["size_impact"] = processed_size / unprocessed_size
+
+    processed_similarity = processed_result["similarity_to_original"]
+    unprocessed_similarity = unprocessed_result["similarity_to_original"]
+    format_result["similarity_impact"] = processed_similarity / unprocessed_similarity
+
+
+def get_format_result(format_path, flag_unprocessed, original_data):
+    format_result = {}
+
+    # Input Image ---> Processed Image ---> PNG/JPG/QOI
+    write_partial_result(format_path, PartialResultType.PROCESSED, original_data, format_result)
+
+    # Input Image ---> PNG/JPG/QOI
+    if flag_unprocessed:
+        write_partial_result(format_path, PartialResultType.UNPROCESSED, original_data, format_result)
+        write_partial_result_comparison(format_result)
+
+    return format_result
 
 
 def benchmark(input_path, output_path, time, formats, flag_unprocessed):
-    result = {}
+    benchmark_result = {}
     
     # Base Information
-    _, original_image = load_image(input_path)
+    original_image = get_image_tensor(input_path)
     original_size = os.path.getsize(input_path)
+    original_data = (original_image, original_size)
     original_object = {
         "path": input_path,
         "size": original_size,
         "time": time
     }
-    result["original"] = original_object
+    benchmark_result["original"] = original_object
 
     # Format Specific Information
-    if ExportFormat.JPG in formats:
-        jpg_object = get_format_object(original_image, original_size, output_path, "jpg", flag_unprocessed)
-        result["jpg"] = jpg_object
- 
-    if ExportFormat.PNG in formats:
-        png_object = get_format_object(original_image, original_size, output_path, "png", flag_unprocessed)
-        result["png"] = png_object
+    for format in formats:
+        format_name = str(format).lower()
+        format_path = f"{output_path}_{{0}}.{format_name}"
+        format_result = get_format_result(format_path, flag_unprocessed, original_data)
+        benchmark_result[format_name] = format_result
 
-    if ExportFormat.QOI in formats:
-        qoi_object = get_format_object(original_image, original_size, output_path, "qoi", flag_unprocessed)
-        result["qoi"] = qoi_object
-
-    return result
-
-
-def get_format_object(original_image, original_size, output_path, format, flag_unprocessed):
-    format_object = {}
-
-    # Input Image ---> Processed Image ---> PNG/JPG/QOI
-    processed_path = f"{output_path}_processed.{format}"
-    _, processed_image = load_image(processed_path)
-    processed_size = os.path.getsize(processed_path)
-    processed_uqi = uqi(original_image, processed_image)
-    processed_object = {
-        "path": processed_path,
-        "size": processed_size,
-        "size_ratio_to_original": processed_size / original_size,
-        "uqi_similarity_index": processed_uqi
-    }
-    format_object["processed"] = processed_object
-
-    if flag_unprocessed:
-        # Input Image ---> PNG/JPG/QOI
-        unprocessed_path = f"{output_path}_unprocessed.{format}"
-        _, unprocessed_image = load_image(unprocessed_path)
-        unprocessed_size = os.path.getsize(unprocessed_path)
-        unprocessed_uqi = uqi(original_image, unprocessed_image)
-        unprocessed_object = {
-            "path": unprocessed_path,
-            "size": unprocessed_size,
-            "size_ratio_to_original": unprocessed_size / original_size,
-            "uqi_similarity_index": unprocessed_uqi
-        }
-        format_object["unprocessed"] = unprocessed_object
-
-        # At this point, compression happens with and without
-        # previous triangulation. Its impact hence can be determined.
-        format_object["size_impact"] = processed_size / unprocessed_size
-        format_object["similarity_impact"] = processed_uqi / unprocessed_uqi
-
-    return format_object
+    return benchmark_result
